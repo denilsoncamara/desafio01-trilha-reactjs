3e33ac960595b7a21ae43331694a5c3a3389248e
Denilson Camara <denilson@tentaculo.digital>
2021-03-30 20:38:05 -0300

    feat: update task status

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -29,6 +29,12 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const tasksUpdatted = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(tasksUpdatted);
   }
 
   function handleRemoveTask(id: number) {
